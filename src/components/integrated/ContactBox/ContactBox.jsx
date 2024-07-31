099c9a661a7f0273817a07c18ad0c5aa3130ecd6
Vicente D'Alessandro <vicente.d.dalessandro@gmail.com>
2024-07-31 18:49:04 -0300

    Comentamos temporalmente el ReCaptcha

--- src/components/integrated/ContactBox/ContactBox.jsx
+++ src/components/integrated/ContactBox/ContactBox.jsx
@@ -1,47 +1,48 @@
 import './ContactBox.css'
-import { useRef, useState } from 'react'
-import ReCAPTCHA from 'react-google-recaptcha'
-import emailjs from '@emailjs/browser'
+// import { useRef, useState } from 'react'
+// import ReCAPTCHA from 'react-google-recaptcha'
+// import emailjs from '@emailjs/browser'
 import Button from '../../core/Button/Button'
 
 function ContactBox () {
-  const SITEKEY = import.meta.env.VITE_SITEKEY_RECAPTCHA
-  const EJS_SERV_ID = import.meta.env.VITE_EJS_SERV_ID
-  const EJS_TEMP_ID = import.meta.env.VITE_EJS_TEMP_ID
-  const EJS_PUBLIC_KEY = import.meta.env.VITE_EJS_PUBLIC_KEY
-  const form = useRef()
-  const [isCaptchaVerified, setIsCaptchaVerified] = useState(false)
-  const [isSucces, setIsSucces] = useState()
+  // const SITEKEY = import.meta.env.VITE_SITEKEY_RECAPTCHA
+  // const EJS_SERV_ID = import.meta.env.VITE_EJS_SERV_ID
+  // const EJS_TEMP_ID = import.meta.env.VITE_EJS_TEMP_ID
+  // const EJS_PUBLIC_KEY = import.meta.env.VITE_EJS_PUBLIC_KEY
+  // const form = useRef()
+  // const [isCaptchaVerified, setIsCaptchaVerified] = useState(false)
+  // const [isSucces, setIsSucces] = useState()
 
-  const handleChange = (e) => {
-    e === null ? setIsCaptchaVerified(false) : setIsCaptchaVerified(true)
-  }
+  // const handleChange = (e) => {
+  //   e === null ? setIsCaptchaVerified(false) : setIsCaptchaVerified(true)
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
 
-    if (isCaptchaVerified) {
-      emailjs.sendForm(EJS_SERV_ID, EJS_TEMP_ID, form.current, {
-        publicKey: EJS_PUBLIC_KEY
-      })
-        .then(
-          () => {
-            console.log('SUCCESS!', form.current)
-            setIsSucces(true)
-          },
-          (error) => {
-            console.log('FAILED...', error.text)
-            setIsSucces(false)
-          }
-        )
-    }
-  }
+  //   if (isCaptchaVerified) {
+  //     emailjs.sendForm(EJS_SERV_ID, EJS_TEMP_ID, form.current, {
+  //       publicKey: EJS_PUBLIC_KEY
+  //     })
+  //       .then(
+  //         () => {
+  //           console.log('SUCCESS!', form.current)
+  //           setIsSucces(true)
+  //         },
+  //         (error) => {
+  //           console.log('FAILED...', error.text)
+  //           setIsSucces(false)
+  //         }
+  //       )
+  //   }
+  // }
 
   return (
     <>
       <div className='ContactBox' id='contact'>
         <h4 className='ContactBox-title'>CONTACTO</h4>
-        <form ref={form} className='ContactBox-form' action='' method='POST' onSubmit={handleSubmit}>
+        {/* ref={form} onSubmit={handleSubmit} */}
+        <form className='ContactBox-form' action='' method='POST'>
           <div className='ContactBox-inputs'>
             <input
               className='ContactBox-input'
@@ -73,9 +74,9 @@ function ContactBox () {
               required
             />
           </div>
-          <div className='ContactBox-reCaptcha'>
+          {/* <div className='ContactBox-reCaptcha'>
             <ReCAPTCHA sitekey={SITEKEY} onChange={handleChange} theme='dark' />
-          </div>
+          </div> */}
           {/* <div className='ContactBox-notificationBox'>
             {!isCaptchaVerified
               ? (<span className='ContactBox-notification ContactBox-notification--reCaptcha'>¡Realiza la Verificación ReCaptcha para enviar la consulta!</span>)
