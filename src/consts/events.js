cb3398f67d07b3f60dd46aa12e54a024057a5334
Manuel Seisdedos <seisdedosmanuel2@gmail.com>
2024-10-09 19:29:02 -0300

    Se agrega el evento del dia 12/10

--- src/consts/events.js
+++ src/consts/events.js
@@ -3,6 +3,25 @@ import { v4 as uuidv4 } from 'uuid'
 const events = [
   {
     uuid: uuidv4(),
+    date: 'OCT 12/24',
+    hour: '23:00hs',
+    location: {
+      adress: {
+        place: 'Fungus Beer',
+        street: {
+          name: 'Calle 73',
+          number: 'n1279'
+        }
+      },
+      city: 'La Plata',
+      province: 'Buenos Aires',
+      googleMapsLink: 'https://www.google.com.ar/maps/place/Fungus/@-34.9149735,-57.9889486,14z/data=!4m10!1m2!2m1!1sFungus+Beer+La+Plata!3m6!1s0x95a2e7f479bccf77:0x14818d3ab4a9e53d!8m2!3d-34.9207452!4d-57.9433664!15sChRGdW5ndXMgQmVlciBMYSBQbGF0YVoWIhRmdW5ndXMgYmVlciBsYSBwbGF0YZIBA2JhcuABAA!16s%2Fg%2F11stgfzhd6?entry=ttu',
+      googleMapsIframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26173.282394426406!2d-57.98894856756369!3d-34.91497349798219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e7f479bccf77%3A0x14818d3ab4a9e53d!2sFungus!5e0!3m2!1ses!2sar!4v1722454725362!5m2!1ses!2sar'
+    },
+    ticketUrl: 'https://www.instagram.com/fungus.beer/'
+  },
+  {
+    uuid: uuidv4(),
     date: 'SEP 06/24',
     hour: '23:00hs',
     location: {
@@ -38,25 +57,6 @@ const events = [
       googleMapsIframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26173.282394426406!2d-57.98894856756369!3d-34.91497349798219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e7f479bccf77%3A0x14818d3ab4a9e53d!2sFungus!5e0!3m2!1ses!2sar!4v1722454725362!5m2!1ses!2sar'
     },
     ticketUrl: 'https://www.instagram.com/fungus.beer/'
-  },
-  {
-    uuid: uuidv4(),
-    date: 'JUN 15/24',
-    hour: '00:00hs',
-    location: {
-      adress: {
-        place: 'Casa Harem Del Fortin',
-        street: {
-          name: 'Calle 68',
-          number: 'e/9 y 10'
-        }
-      },
-      city: 'La Plata',
-      province: 'Buenos Aires',
-      googleMapsLink: 'https://www.google.com.ar/maps/place/Casa+Harem/@-34.9317471,-57.9334981,15z/data=!4m14!1m7!3m6!1s0x95a2e97f91769d3b:0x4688061df0599fdc!2sCasa+Harem!8m2!3d-34.9298923!4d-57.935094!16s%2Fg%2F11hds1cysz!3m5!1s0x95a2e97f91769d3b:0x4688061df0599fdc!8m2!3d-34.9298923!4d-57.935094!16s%2Fg%2F11hds1cysz?entry=ttu',
-      googleMapsIframe: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13083.96648927495!2d-57.93349806529876!3d-34.93174706423865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e97f91769d3b%3A0x4688061df0599fdc!2sCasa%20Harem!5e0!3m2!1ses!2sar!4v1722454156543!5m2!1ses!2sar'
-    },
-    ticketUrl: 'https://www.instagram.com/p/C7MT3SfAP1e/'
   }
 ]
 
